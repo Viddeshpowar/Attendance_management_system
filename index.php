7a782680050f75c530afff10fbb4bc9a48d13e02
ViddeshPowar <viddeshpowar@gmail.com>
2021-12-29 13:20:31 +0530

    attendance management system

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'modules/header.php';?>
+    
+ <?php include 'modules/nav.php';?>
+<?php include 'modules/content-container.php';?>
+
+<?php
+$con = mysqli_connect('localhost','root','','studentattdb');?>
+
+
+
+<?php include 'modules/footer.php';?>
